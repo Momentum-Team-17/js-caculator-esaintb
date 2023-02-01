0a188745875b94a2c11e90bcccdce3c31a46bf02
esantb <e.saint.bou@gmail.com>
2023-02-01 14:21:00 -0600

    clear button working

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
+
+// click response & placing variables in the display area 
 let buttons = document.querySelectorAll(".button")
-const screen = document.querySelector('#container')
+const displayScreen = document.querySelector('#container')
 console.log(buttons)
 
 const fullDisplay = [];
@@ -8,7 +10,23 @@ for (let button of buttons) {
         console.log(event.target.innerText)
         let buttonValue = event.target.innerText;
         fullDisplay.push(buttonValue);
-        screen.innerText = fullDisplay.join("");
+        displayScreen.innerText = fullDisplay.join("");
     })
 }
 
+// = .js
+// apply eval to contents of display
+const equalButton = document.querySelector("#equals");
+equalButton.addEventListener('click', function (event) {
+    // make clicking the equals button evaluate the expression
+    // in the displayScreen and display the solution
+    const answer = eval(displayScreen.innerText)
+    console.log(answer);
+    displayScreen.innerText = answer; 
+})
+
+// clear.js
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener('click', function (event) {
+    displayScreen.innerText = "";
+})
